a009092c234cafce9748f5d4ce7f762b524dae4c
Gopal <raunakchd1@gmail.com>
2025-04-23 22:17:06 +0530

    removed build command meant for serving frontend

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 app.use("/api",api);
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.get('*', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
 app.listen(port, () => {
     console.log(`Server running at ${port}`); 
